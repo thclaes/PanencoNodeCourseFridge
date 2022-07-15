d283930fd3d79c6ebab8a5dd35c6bcd5f19b8993
CasMagnus1 <cas.magnus@student.kuleuven.be>
2022-07-15 15:40:53 +0200

    added capacity limitation + test

--- example1/src/controllers/products/handlers/createProduct.handler.ts
+++ example1/src/controllers/products/handlers/createProduct.handler.ts
@@ -1,4 +1,5 @@
 import { RequestContext } from "@mikro-orm/core";
+import { Conflict, NotFound } from "@panenco/papi";
 import { ProductBody } from "../../../contracts/product/product.body";
 import { Fridge } from "../../../entities/fridge.entity";
 import { Product } from "../../../entities/product.entity";
@@ -8,11 +9,20 @@ import { get } from "../../users/handlers/getUser.handler";
 
 export const createProduct = async (body: ProductBody): Promise<Product> => {
   const em = RequestContext.getEntityManager();
-
+  
   const {userId, fridgeId, ...bodyWithoutFK} = body;
+
+  const fridge = await getFridge(fridgeId);
+  const products = Array.from(fridge.products);
+  let totalContent = 0;
+  products.forEach(product => {
+    totalContent += product.size;
+  });
+  if (totalContent + bodyWithoutFK.size > fridge.capacity) throw new Conflict("fridge full", "fridge full");
+
   const product = em.create(Product, bodyWithoutFK);
   product.owner = await get(userId);
-  product.fridge = await getFridge(fridgeId);
+  product.fridge = fridge;
 
   await em.persistAndFlush(product);
 
