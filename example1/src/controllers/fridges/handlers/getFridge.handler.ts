50660709a5b7188d8feed396c4a9f71712dc447f
ThomasClaes-r0664186 <thomas.claes@student.ucll.be>
2022-07-18 11:47:30 +0200

    Done

--- example1/src/controllers/fridges/handlers/getFridge.handler.ts
+++ example1/src/controllers/fridges/handlers/getFridge.handler.ts
@@ -2,6 +2,6 @@ import { RequestContext } from "@mikro-orm/core";
 import { Fridge } from "../../../entities/fridge.entity";
 
 export const getFridge = async (id: string): Promise<Fridge> => {
-    const em = RequestContext.getEntityManager();
-    return await em.findOneOrFail(Fridge,{ id }, { populate: ['products'] });
+  const em = RequestContext.getEntityManager();
+  return em.findOneOrFail(Fridge, { id }, { populate: ["products"] });
 };
--- example1/src/controllers/products/handlers/createProduct.handler.ts
+++ example1/src/controllers/products/handlers/createProduct.handler.ts
@@ -1,26 +1,34 @@
 import { RequestContext } from "@mikro-orm/core";
-import { Conflict, NotFound } from "@panenco/papi";
+import { Conflict } from "@panenco/papi";
 import { ProductBody } from "../../../contracts/product/product.body";
+import { Fridge } from "../../../entities/fridge.entity";
 import { Product } from "../../../entities/product.entity";
 import { getFridge } from "../../fridges/handlers/getFridge.handler";
 import { get } from "../../users/handlers/getUser.handler";
 
-export const createProduct = async (body: ProductBody, userId: string): Promise<Product> => {
+export const createProduct = async (
+  body: ProductBody,
+  userId?: string
+): Promise<Product> => {
   const em = RequestContext.getEntityManager();
-  
-  const {fridgeId, ...bodyWithoutFK} = body;
 
-  const fridge = await getFridge(fridgeId);
-  const products = Array.from(fridge.products);
-  let totalContent = 0;
-  products.forEach(product => {
-    totalContent += product.size;
-  });
-  if (totalContent + bodyWithoutFK.size > fridge.capacity) throw new Conflict("fridge full", "fridge full");
+  const { fridgeId, ...bodyWithoutFK } = body;
+  let fridge: Fridge;
+
+  if (fridgeId) {
+    fridge = await getFridge(fridgeId);
+    const products = Array.from(fridge.products);
+    let totalContent = 0;
+    products.forEach((product) => {
+      totalContent += product.size;
+    });
+    if (totalContent + bodyWithoutFK.size > fridge.capacity)
+      throw new Conflict("fridge full", "fridge full");
+  }
 
   const product = em.create(Product, bodyWithoutFK);
-  product.owner = await get(userId);
-  product.fridge = fridge;
+  if (userId) product.owner = await get(userId);
+  if (fridge) product.fridge = fridge;
 
   await em.persistAndFlush(product);
 
